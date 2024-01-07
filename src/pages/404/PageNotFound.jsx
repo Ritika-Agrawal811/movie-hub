c43a0cadc629ecf9908f1008358cb7aaa0ce4965
Ritika Agrawal <ritikaagrawal339@gmail.com>
2024-01-07 18:20:32 +0530

    refactor: re-style and refactor 404 page

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -6,8 +6,7 @@ import "./style.scss"
 import ContentWrapper from "@/components/contentWrapper/ContentWrapper"
 import SearchField from "@/components/searchField/SearchField"
 import Image from "@/components/lazyLoadImage/Image"
-import rightPlugImage from "@/assets/right-plug.png"
-import leftPlugImage from "@/assets/left-plug.png"
+import notFoundImage from "@/assets/not-found.png"
 
 const PageNotFound = () => {
     const [query, setQuery] = useState("")
@@ -22,12 +21,14 @@ const PageNotFound = () => {
     return (
         <section className='error__page'>
             <ContentWrapper>
-                <h2 className='error__title'>404</h2>
-                <div className='error__subBlock'>
-                    <Image src={leftPlugImage} alt='left plug' className='left-plug' />
-                    <h3 className='error__subtitle'>not found</h3>
-                    <Image src={rightPlugImage} alt='right plug' className='right-plug' />
-                </div>
+                <figure className='error__image-container'>
+                    <Image src={notFoundImage} className='error__image' />
+                </figure>
+                <h2 className='error__title'>Page Not Found</h2>
+                <p className='error__subtitle'>
+                    Oops! Looks like the page you are looking for doesn&apos;t exist. Search for a movie or a TV show
+                    from the search bar.
+                </p>
 
                 <section className='search__container'>
                     <SearchField query={query} setQuery={setQuery} searchQueryHandler={searchQueryHandler} />
